a45617c078f70818bc4c479572d713f300fbae87
Park,JongBum <jbpark03@gmail.com>
2021-07-22 21:38:23 +0900

    instead of pathvariable, requestparam is used
    end point : rest?roadBldgName=덕영대로895

--- src/com/jbpark/dabang/controller/AddressController.java
+++ src/com/jbpark/dabang/controller/AddressController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jbpark.dabang.domain.AddressRow;
@@ -19,20 +20,18 @@ public class AddressController {
 	private AddressService addressService;
 	
 	//@formatter:off
-	@RequestMapping(value={"/{road_bldgName}",
-			"/{road_bldgName}/{bldgNum}"}, 
-			method=RequestMethod.GET)
+	@RequestMapping(value="read", method=RequestMethod.GET)
 	public List<AddressRow> read(
-			@PathVariable(name="road_bldgName") String road_bldgName,
-			@PathVariable(name="bldgNum", required = false) String bldgNum) {
+			@RequestParam(required = true) String roadBldgName,
+			@RequestParam(required = false) String bldgNum
+			) {
 		AddrSearchKey searchKey = new AddrSearchKey();
 		
-		searchKey.set도로_건물(road_bldgName);
+		searchKey.set도로_건물(roadBldgName);
 		if (bldgNum != null && bldgNum.trim().length() > 0) {
 			searchKey.set건물본번(bldgNum.trim());
 		}
 		
 		return addressService.read(searchKey);
 	}
-
 }
